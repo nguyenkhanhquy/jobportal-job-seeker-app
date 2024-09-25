354dede5b0e5b9e6cd4c832572dbb01b5cb26560
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-09-26 00:01:28 +0700

    add: intro screen

---
+++ app/screens/intro/intro.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { Image, StyleSheet, View } from "react-native";
+// import { introspect } from "../../services/AuthAPIService";
+// import { getToken } from "../../utils/AuthStorage";
+
+import logo from "../../assets/img/logo.png";
+
+export default function Intro({ navigation }) {
+    // useEffect(() => {
+    //     const checkToken = async () => {
+    //         const token = await getToken();
+    //         if (token) {
+    //             const data = await introspect(token);
+
+    //             if (data.success) {
+    //                 navigation.replace("Home");
+    //                 return;
+    //             }
+    //         }
+    //         navigation.replace("Start");
+    //     };
+
+    //     const timer = setTimeout(() => {
+    //         checkToken();
+    //     }, 1200);
+
+    //     return () => clearTimeout(timer);
+    // }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <Image source={logo} style={styles.image} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    image: {
+        width: 120,
+        height: 120,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+});
