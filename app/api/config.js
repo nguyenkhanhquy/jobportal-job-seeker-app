f33fc69c29e9484c1ba8d84f145f428243443254
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-26 09:24:42 +0700

    add: api config

---
+++ app/api/config.js
@@ -0,0 +1,9 @@
+export const AUTH_API = {
+    REGISTER: "/auth/register",
+    LOGIN: "/auth/login",
+    LOGOUT: "/auth/logout",
+};
+
+export const USERS_API = {
+    MY_INFO: "/users/my-info",
+};
