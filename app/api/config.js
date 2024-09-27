37cb46faf147c93e9748a0d6689ac284127fd1e7
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-27 23:34:07 +0700

    update: config.js
    add JOBS_API and JOB_SEEKER_API

--- app/api/config.js
+++ app/api/config.js
@@ -13,3 +13,14 @@ export const AUTH_API = {
 export const USERS_API = {
     MY_INFO: "/users/my-info",
 };
+
+export const JOBS_API = {
+    LIST: "/jobs",
+    DETAILS: "/jobs/:id",
+    APPLY: "/jobs/:id/apply",
+};
+
+export const JOB_SEEKER_API = {
+    UPDATE_AVATAR: "/job-seeker/update-avatar",
+    UPDATE_PROFILE: "/job-seeker/update-profile",
+};
