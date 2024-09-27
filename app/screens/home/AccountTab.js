8eea325230f81c7f950baa5fd76d85db48df6f57
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-28 01:38:37 +0700

    add: account tab

--- app/screens/home/AccountTab.js
+++ app/screens/home/AccountTab.js
@@ -1,27 +1,211 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity, Alert, Image } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Octicons from "@expo/vector-icons/Octicons";
+import { StatusBar } from "expo-status-bar";
+import * as ImagePicker from "expo-image-picker";
 
-const AccountTab = () => {
+// import { logout } from "../../services/authAPIService";
+// import { myInfo } from "../../services/usersAPIService";
+// import { updateAvatar } from "../../services/jobSeekerAPIService";
+
+import { getToken, deleteToken } from "../../utils/authStorage";
+
+const AccountTab = ({ navigation }) => {
+    const [loading, setLoading] = useState(true);
+    const [userInfo, setUserInfo] = useState(null);
+
+    const fetchUserInfo = useCallback(async () => {
+        try {
+            setLoading(true);
+            const token = await getToken();
+            if (token) {
+                const data = await myInfo(token);
+                if (data.success) {
+                    setUserInfo(data.result);
+                } else {
+                    Alert.alert("Error", data.message);
+                }
+            }
+        } catch (error) {
+            Alert.alert("Error", "Failed to fetch user information.");
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchUserInfo();
+        }, [])
+    );
+
+    // const handleLogout = async () => {
+    //     Alert.alert(
+    //         "Xác nhận đăng xuất",
+    //         "Bạn có chắc chắn muốn đăng xuất?",
+    //         [
+    //             {
+    //                 text: "Hủy",
+    //             },
+    //             {
+    //                 text: "Đăng xuất",
+    //                 onPress: async () => {
+    //                     try {
+    //                         setLoading(true);
+    //                         const token = await getToken();
+    //                         if (token) {
+    //                             const data = await logout(token);
+
+    //                             if (data.success) {
+    //                                 deleteToken();
+    //                                 setUserInfo(null);
+    //                                 navigation.navigate("Login");
+    //                                 Alert.alert("Success", data.message);
+    //                             }
+    //                         }
+    //                     } catch (error) {
+    //                         Alert.alert("Logout failed", "An error occurred. Please try again.");
+    //                     } finally {
+    //                         setLoading(false);
+    //                     }
+    //                 },
+    //             },
+    //         ],
+    //         { cancelable: false }
+    //     );
+    // };
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <StatusBar style="auto" />
+                <ActivityIndicator size="large" color="#16a34a" />
+            </View>
+        );
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Account</Text>
+        <View className="flex-1 bg-gray-100">
+            <StatusBar style="auto" />
+            {/* Background Section */}
+            <View className="bg-[#5fa75f] h-36 w-full absolute top-0 left-0 right-0 z-[-1]" />
+
+            {/* Profile Section */}
+            {userInfo ? (
+                <>
+                    <View className="flex-row bg-white rounded-lg p-5 mx-5 mt-20" style={styles.shadowStyle}>
+                        <View className="relative">
+                            {userInfo.avatar ? (
+                                <Image
+                                    source={{
+                                        uri: userInfo.avatar,
+                                    }}
+                                    className="w-24 h-24 rounded-full border-2 border-[#6dcf5b]"
+                                />
+                            ) : (
+                                <View className="w-24 h-24 rounded-full border-2 border-[#509b43] justify-center items-center">
+                                    <Ionicons name="person-outline" size={48} color="#509b43" />
+                                </View>
+                            )}
+                            <TouchableOpacity
+                                className="absolute right-0 bottom-0 bg-gray-600 rounded-full p-1 border-2 border-white"
+                                // onPress={selectImage}
+                            >
+                                <Ionicons name="camera-outline" size={20} color="#fff" />
+                            </TouchableOpacity>
+                        </View>
+                        <View className="flex-1 ml-4">
+                            <Text className="text-lg font-bold text-gray-800 mb-1">{userInfo.fullName}</Text>
+                            <Text className="text-sm text-gray-600" numberOfLines={1} ellipsizeMode="tail">
+                                {userInfo.email}
+                            </Text>
+                            {userInfo.active ? (
+                                <View className="flex-row items-center mt-2">
+                                    <Octicons
+                                        name="shield-check"
+                                        size={20}
+                                        color="#6dcf5b"
+                                        style={{ marginRight: 4 }}
+                                    />
+                                    <Text className="text-sm font-bold text-gray-600">Tài khoản đã xác thực</Text>
+                                </View>
+                            ) : (
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        navigation.navigate("ActivateAccount", { email: userInfo.email });
+                                    }}
+                                >
+                                    <View className="flex-row items-center mt-2">
+                                        <Octicons name="shield-x" size={20} color="red" style={{ marginRight: 4 }} />
+                                        <Text className="text-sm font-bold text-gray-600">Tài khoản chưa xác thực</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            )}
+                        </View>
+                    </View>
+
+                    {/* Options Section */}
+                    <View className="px-5 mt-10">
+                        <TouchableOpacity
+                            className="bg-white p-3 rounded-lg mb-4"
+                            style={styles.shadowStyle}
+                            onPress={() => navigation.navigate("Profile", { user: userInfo })}
+                        >
+                            <Text className="text-lg font-medium text-gray-800">Thông tin cá nhân</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            className="bg-white p-3 rounded-lg mb-4"
+                            style={styles.shadowStyle}
+                            onPress={() => navigation.navigate("ChangePassword", { user: userInfo })}
+                        >
+                            <Text className="text-lg font-medium text-gray-800">Đổi mật khẩu</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            className="bg-white p-3 rounded-lg"
+                            style={styles.shadowStyle}
+                            // onPress={handleLogout}
+                        >
+                            <Text className="text-lg font-medium text-red-600">Đăng xuất</Text>
+                        </TouchableOpacity>
+                    </View>
+                </>
+            ) : (
+                <>
+                    <View className="flex-row bg-white rounded-lg p-5 mx-5 mt-20" style={styles.shadowStyle}>
+                        <View className="relative mr-5">
+                            <View className="w-24 h-24 rounded-full border-2 border-[#509b43] justify-center items-center">
+                                <Ionicons name="person-outline" size={48} color="#509b43" />
+                            </View>
+                        </View>
+                        <View className="flex-1 justify-center">
+                            <Text className="text-lg font-bold text-gray-800 mb-3 text-center">Vui lòng đăng nhập</Text>
+                            <TouchableOpacity
+                                className="bg-[#509b43] p-3 rounded-lg justify-center items-center"
+                                onPress={() => {
+                                    navigation.navigate("Login");
+                                }}
+                            >
+                                <Text className="text-lg font-medium text-white">Đăng nhập</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </>
+            )}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#eafaf1",
-        justifyContent: "center",
-        paddingHorizontal: 20,
-    },
-    title: {
-        fontSize: 30,
-        fontWeight: "bold",
-        color: "#6dcf5b",
-        marginBottom: 30,
-        textAlign: "center",
+    shadowStyle: {
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 5,
     },
 });
 
