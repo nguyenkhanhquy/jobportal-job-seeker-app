d19b82fdcf0427120d736fa0c55f3cbb83c13153
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-27 23:17:27 +0700

    update: config auth api

--- app/api/config.js
+++ app/api/config.js
@@ -2,6 +2,12 @@ export const AUTH_API = {
     REGISTER: "/auth/register",
     LOGIN: "/auth/login",
     LOGOUT: "/auth/logout",
+    SEND_OTP: "/auth/send-otp",
+    VALIDATE_OTP: "/auth/validate-otp",
+    RESET_PASSWORD: "/auth/reset-password",
+    INTROSPECT: "/auth/introspect",
+    ACTIVATE: "/auth/activate",
+    UPDATE_PASSWORD: "/auth/update-password",
 };
 
 export const USERS_API = {
