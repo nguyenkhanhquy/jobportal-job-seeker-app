06e33905ea9e9aa3c9c90e0815faf673adf28a33
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-09-30 01:06:16 +0700

    update: border job card

--- app/components/JobCard.js
+++ app/components/JobCard.js
@@ -3,7 +3,10 @@ import { TouchableOpacity, View, Image, Text } from "react-native";
 
 const JobCard = ({ job, onPress }) => {
     return (
-        <TouchableOpacity onPress={onPress} className="flex-row bg-white p-4 rounded-lg mb-2 mr-4 shadow-sm">
+        <TouchableOpacity
+            onPress={onPress}
+            className="flex-row bg-white p-4 rounded-lg mb-2 mr-4 shadow-sm border border-gray-100"
+        >
             <Image source={{ uri: job.logo }} className="w-16 h-16 rounded-lg mr-4" />
             <View className="flex-1">
                 <Text className="text-lg font-bold text-gray-900 mb-1">{job.title}</Text>
