a36743ae8619e9293f8a66c8cbf0ec32a17ec247
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-10-02 00:21:03 +0700

    update: Carousel.js

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
 
-const Carousel = ({ data, renderItem }) => {
+const Carousel = ({ data, renderItem, horizontal = true }) => {
     return (
         <View style={styles.carouselContainer}>
             <FlatList
                 data={data}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id.toString()}
-                horizontal
-                showsHorizontalScrollIndicator
+                horizontal={horizontal}
+                showsHorizontalScrollIndicator={horizontal}
+                showsVerticalScrollIndicator={!horizontal}
                 snapToInterval={350}
                 snapToAlignment="start"
                 decelerationRate="fast"
