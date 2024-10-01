dee0b2444853472cbd6bfc269c69aafa721cf9fd
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-02 01:36:05 +0700

    update: job api service

--- app/services/jobAPIService.js
+++ app/services/jobAPIService.js
@@ -1,6 +1,11 @@
 import { getRequest } from "../api/request";
 import { JOBS_API } from "../api/config";
 
-export const getListJobs = async () => {
-    return getRequest(JOBS_API.LIST);
+export const getListJobs = async (page, size) => {
+    return getRequest(JOBS_API.LIST, {
+        params: {
+            page: page ?? 1,
+            size: size ?? 5,
+        },
+    });
 };
