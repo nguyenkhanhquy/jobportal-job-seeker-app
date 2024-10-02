e3b3f67c3bb0ad4063d36466b5d118736e53ae2f
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-10-03 02:11:14 +0700

    update: SearchBar.js

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { View, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const SearchBar = ({ onSearchPress }) => {
+const SearchBar = ({ onSearch }) => {
+    const [query, setQuery] = useState("");
+
+    const handleChange = (text) => {
+        setQuery(text);
+        onSearch(text);
+    };
+
     return (
         <View className="flex-row items-center bg-gray-100 rounded-lg py-3 px-4 mx-5 shadow-sm">
             <Ionicons name="search" size={24} color="#888" className="mr-3" />
-            <TouchableOpacity className="flex-1" onPress={onSearchPress}>
-                <TextInput className="text-base text-gray-700" placeholder="Tìm kiếm công việc" editable={false} />
-            </TouchableOpacity>
+            <TextInput
+                className="flex-1 text-base text-gray-700"
+                placeholder="Tìm kiếm công việc"
+                value={query}
+                onChangeText={handleChange}
+            />
         </View>
     );
 };
