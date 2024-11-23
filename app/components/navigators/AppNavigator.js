e3eca35bfa8e0fff308e7b891397e59e0951409a
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-24 01:40:00 +0700

    add: job application screen

--- app/components/navigators/AppNavigator.js
+++ app/components/navigators/AppNavigator.js
@@ -13,6 +13,7 @@ import ActivateAccount from "../../screens/account/activateAccount";
 import Profile from "../../screens/account/profile";
 import ChangePassword from "../../screens/account/changePassword";
 import JobDetail from "../../screens/job/JobDetail";
+import JobApplicationScreen from "../../screens/job/JobApplicationScreen/JobApplicationScreen";
 import JobList from "../../screens/job/JobList";
 
 const Stack = createStackNavigator();
@@ -45,6 +46,17 @@ const AppNavigator = () => {
                     options={{ headerShown: true, headerTitle: "Đổi mật khẩu", headerTitleAlign: "center" }}
                 />
                 <Stack.Screen name="JobDetail" component={JobDetail} />
+                <Stack.Screen
+                    name="JobApplication"
+                    component={JobApplicationScreen}
+                    options={{
+                        headerShown: true,
+                        headerTitle: "Ứng tuyển",
+                        headerTitleAlign: "center",
+                        headerStyle: { backgroundColor: "#ffffff", borderColor: "#f0f0f0", borderBottomWidth: 1 },
+                        headerTintColor: "#000",
+                    }}
+                />
                 <Stack.Screen
                     name="JobList"
                     component={JobList}
